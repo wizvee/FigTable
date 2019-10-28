673e878df4ec29c580f13823302c7b158e453342
onethegarden <onethegarden@gmail.com>
2019-10-28 21:46:51 +0900

    clear :

--- src/main/react/components/pages/eatdeal/EatdealContainer.js
+++ src/main/react/components/pages/eatdeal/EatdealContainer.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import HeaderSimple from '../../common/HeaderSimple';
+import EatdealMainContainer from './EatdealMainContainer';
+
+
+
+const sample = [
+  {
+    id: 1,
+    thumb:
+      'https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/20190623074633_photo1_a8KtahP0JSRT.jpg',
+    title: '달콩카페',
+    status:'N',//new
+    discount:'20%',
+    originPrice:25000,
+    discountPrice:20000,
+    remainFood:3,
+    FoodName:'녹차케이크',
+  },
+  {
+    id: 2,
+    thumb:
+      'https://mp-seoul-image-production-s3.mangoplate.com/added_restaurants/52193_1488438243054735.jpg',
+    title: '아이엠바리스타',
+    status:'Y',//재입고
+    discount:'40%',
+    originPrice:25000,
+    discountPrice:20000,
+    remainFood:50,
+    FoodName:'녹차케이크',
+  },
+  {
+    id: 3,
+    thumb:
+      'https://mp-seoul-image-production-s3.mangoplate.com/819837_1509504944362416.jpg',
+    title: '나이트티',
+    status:'N',
+    discount:'20%',
+    originPrice:25000,
+    discountPrice:20000,
+    remainFood:5,
+    FoodName:'녹차케이크',
+  },
+];
+
 
 const EatdealContainer = () => {
+  const [eatDeals, setEatDeals] = useState(sample);
+
+  
   return (
     <>
       <HeaderSimple />
-      <div>여기는 잇딜</div>
+      <div>
+        {eatDeals.map(eatDeal=>(
+          <EatdealMainContainer key={eatDeal.id} eatDeal={eatDeal}/>
+        ))};
+      </div>
+      
     </>
   );
 };
