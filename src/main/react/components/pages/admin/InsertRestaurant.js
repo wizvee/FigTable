c2855bdb15e1e97e0246ba4e5cb458b0b3999349
JiMin <yjimini831@gmail.com>
2019-10-29 17:11:26 +0900

    지민커밋

---
+++ src/main/react/components/pages/admin/InsertRestaurant.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminHeader from './AdminHeader';
+
+const InsertRestaurant = () => {
+  return (
+    <>
+      <AdminHeader />
+    </>
+  );
+};
+
+export default InsertRestaurant;
