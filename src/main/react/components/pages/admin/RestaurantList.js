932b1363b5a8a3ed8777d65f3c197e577df9ef9a
JiMin <yjimini831@gmail.com>
2019-11-01 21:50:41 +0900

    테이블에 모달 추가

--- src/main/react/components/pages/admin/RestaurantList.js
+++ src/main/react/components/pages/admin/RestaurantList.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import ModalTemplate from './ModalTemplate';
 
-const RestaurantList = ({ restaurants }) => {
+const RestaurantList = ({ restaurants, match }) => {
+  const { resNo } = match.params;
+  const restaurant = restaurants.find(r => r.resNo === resNo);
+  const [modal, setIsModal] = useState(false);
+
+  const onClickOpenModal = () => {
+    setIsModal(true);
+  };
+
+  const closeModal = () => {
+    setIsModal(false);
+  };
+
+  if (!restaurant) {
+    return <div>존재하지 않습니다.</div>;
+  }
   return (
     <>
       {restaurants.map((row, index) => {
         return (
           <tr key={index}>
-            <td key={`${index}+name`}>{row.resName}</td>
-            <td key={`${index}+addr`}>
-              <Link onClick="">{row.resAddr}</Link>
+            <td key={`${index}+name`}>
+              <Link onClick={() => onClickOpenModal()}>{row.resName}</Link>
+              {!modal ? null : <ModalTemplate />}
             </td>
+            <td key={`${index}+addr`}>{row.resAddr}</td>
             <td key={`${index}+tel`}>{row.resTel}</td>
             <td key={`${index}+own`}>{row.ownName}</td>
           </tr>
@@ -20,4 +37,4 @@ const RestaurantList = ({ restaurants }) => {
   );
 };
 
-export default RestaurantList;
+export default withRouter(RestaurantList);
