8909674d7c557bad96a5cee89859d2ae68509054
nychae <nychae@naver.com>
2019-11-05 15:43:56 +0900

    수정

--- src/main/react/components/pages/owner/OwnerWaitingContainer.js
+++ src/main/react/components/pages/owner/OwnerWaitingContainer.js
@@ -1,12 +1,101 @@
 import React from 'react';
 import HeaderOwner from './HeaderOwner';
+import styled from 'styled-components';
+import palette from '../../../lib/styles/Palette';
+import Responsive from '../../common/Responsive';
+import OwnerDetailTitle from './OwnerDetailTitle';
+import ListContainer from './ListContainer';
 
+const Container = styled.div`
+  padding-top: 80px;
+  height: auto;
+  overflow-y: hidden;
+  background: #f1f3f5;
+  @media (max-width: 425px) {
+    height: 1500px;
+  }
+`;
+
+const ContainerWrapper = styled(Responsive)`
+  height: auto;
+  overflow: hidden;
+  &:after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+  @media (max-width: 425px) {
+    height: 1500px;
+  }
+`;
+
+const Right = styled.div`
+  padding: 1rem;
+  width: 100%;
+  height: 100%;
+  float: right;
+  /* @media (max-width: 768px) {
+    width: 45%;
+  } */
+  @media (max-width: 425px) {
+    width: 100%;
+    position: relative;
+    top: 430px;
+    padding-top: 0.7rem;
+  }
+`;
+const CountContainer = styled.div`
+  margin: 0 auto;
+  margin-top: 30px;
+  height: 60px;
+  width: 45%;
+  font-weight: 500;
+  font-size: 30px;
+  text-align: center;
+  padding-top: 7px;
+  letter-spacing: 7px;
+  border-radius: 5px;
+  z-index: 10;
+  background: rgba(246, 114, 128, 0.7);
+  color: white;
+  .count {
+    color: ${palette.textGray};
+    font-weight: 900;
+  }
+  @media (max-width: 1024px) {
+    letter-spacing: 2px;
+  }
+`;
+
+////////////임시데이터////////////////
 const store = {
   name: '김사장',
 };
 
+const waiting = [
+  { name: '김손님', count: '2' },
+  { name: '이손님', count: '1' },
+  { name: '박손님', count: '5' },
+];
+////////////////////////////////////
+
 const OwnerWaitingContainer = () => {
-  return <HeaderOwner name={store.name} />;
+  return (
+    <>
+      <HeaderOwner name={store.name} />
+      <Container>
+        <ContainerWrapper>
+          <OwnerDetailTitle title="Waiting" />
+          <CountContainer>
+            현재 <span className="count">{waiting.length}</span> 팀 대기 중
+          </CountContainer>
+          <Right>
+            <ListContainer list={waiting} />
+          </Right>
+        </ContainerWrapper>
+      </Container>
+    </>
+  );
 };
 
 export default OwnerWaitingContainer;
--- src/main/react/components/pages/owner/WaitingPublicContainer.js
+++ src/main/react/components/pages/owner/WaitingPublicContainer.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import Responsive from '../../common/Responsive';
+import palette from '../../../lib/styles/Palette';
 
 const Container = styled.div`
+  html {
+    height: 100%;
+  }
+  body {
+    height: 100%;
+  }
   height: 100vmax;
 `;
+// document.body.style.overflow = 'hidden';
+// document.body.style.height = '100%';
 
 const Left = styled(Responsive)`
   float: left;
   width: 50%;
   height: 100%;
   background: url(https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80)
-    no-repeat 0 -30%;
-  background-size: 50vmax 100vmax;
+    no-repeat;
+  background-size: 100%;
+  top: 0;
   padding: 0;
   &.back {
     width: 100%;
@@ -34,9 +44,10 @@ const Content = styled.div`
   z-index: 3;
   font-size: 2rem;
   color: white;
+  padding-top: 4rem;
+  padding-left: 3rem;
 `;
 
-document.body.style.overflow = 'hidden';
 const WaitingPublicContainer = () => {
   return (
     <Container>
@@ -46,6 +57,7 @@ const WaitingPublicContainer = () => {
             휴대폰 번호를 입력하시면
             <br /> 카톡으로 알려드립니다
           </Content>
+          <Count>현재 웨이팅 {count}</Count>
         </Back>
       </Left>
     </Container>
