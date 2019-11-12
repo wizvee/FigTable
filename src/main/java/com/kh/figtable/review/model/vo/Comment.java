40b6fa6d0afd1c394043b12bfdd790ea2d93dede
wizvee <jeonghye.dev@gmail.com>
2019-11-12 12:59:29 +0900

    comment mapper 추가

---
+++ src/main/java/com/kh/figtable/review/model/vo/Comment.java
@@ -0,0 +1,22 @@
+package com.kh.figtable.review.model.vo;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Comment {
+	
+	private String rvcNo;
+	private String rvcNoRef;
+	private String memNo;
+	private String rvcContent;
+	private boolean rvcWarn;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+	private Date rvcDate;
+
+}
