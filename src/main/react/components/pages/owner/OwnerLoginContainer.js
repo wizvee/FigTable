ded5c7d973202d609e719915114cb84b7704a868
nychae <nychae@naver.com>
2019-11-18 08:14:58 +0900

    사장님 로그인페이지  프론트만

--- src/main/react/components/pages/owner/OwnerLoginContainer.js
+++ src/main/react/components/pages/owner/OwnerLoginContainer.js
@@ -1,11 +1,127 @@
 import React from 'react';
 import HeaderOwnerSimple from './common/HeaderOwnerSimple';
+import styled from 'styled-components';
+import Responsive from '../../common/Responsive';
+import palette from '../../../lib/styles/Palette';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  padding-top: 80px;
+  height: 100%;
+  min-height: calc(100vh - 8rem);
+`;
+
+const ContainerWrapper = styled(Responsive)`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 2rem;
+`;
+
+const LoginContainer = styled.div`
+  width: 40%;
+  height: auto;
+  margin: 0 auto;
+  margin-top: 20px;
+`;
+
+const StyledInput = styled.input`
+  padding: 0.5rem 0.8rem;
+  width: 100%;
+  border-radius: 5px;
+  border: 1px solid ${palette.borderGray};
+  font-size: 1rem;
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+const Button = styled.div`
+  width: 100%;
+  background: ${palette.primary};
+  color: white;
+  opacity: 0.8;
+  border-radius: 5px;
+  padding: 0.5rem 0.8rem;
+  margin-top: 1rem;
+  text-align: center;
+  &:hover {
+    opacity: 1;
+    cursor: pointer;
+  }
+`;
+
+const EnrollContainer = styled.div`
+  text-align: center;
+  border: 1px solid ${palette.borderGray};
+  width: 100%;
+  height: auto;
+
+  padding: 20px;
+  border: 1px solid #ced4da;
+  margin-top: 20px;
+
+  .text {
+    font-size: 13px;
+    margin-top: 10px;
+  }
+`;
+
+const EnrollButton = styled.div`
+  width: 65%;
+  padding: 0.3rem 0.8rem;
+  border: 2px solid #868e96;
+  opacity: 0.7;
+  margin: 0 auto;
+  margin-top: 16px;
+  border-radius: 3px;
+  font-size: 15px;
+  &:hover {
+    opacity: 1;
+    cursor: pointer;
+  }
+`;
 
 const OwnerLoginContainer = () => {
+  const path = process.env.PATH;
   return (
     <>
       <HeaderOwnerSimple />
-      <div>로그인</div>
+      <Container>
+        <ContainerWrapper>
+          <h3>로그인</h3>
+          <LoginContainer>
+            <StyledInput
+              autoComplete="ownEmail"
+              name="ownEmail"
+              placeholder="이메일 입력"
+              // value={form.memEmail}
+              // onChange={onChange}
+            />
+            <StyledInput
+              autoComplate="new-password"
+              name="ownPassword"
+              placeholder="비밀번호 입력"
+              type="password"
+              // value={form.memPassword}
+              // onChange={onChange}
+            />
+            <Button>로그인</Button>
+            <EnrollContainer>
+              <b>회원이 아니신가요?</b>
+              <br />
+              <div className="text">
+                회원가입과 동시에 Figtable, Figtable 파트너 사이트의 회원가입이
+                진행되며, 가입 승인 시 Figtable과 Figtable 파트너 사이트를 모두
+                이용하실 수 있습니다.
+              </div>
+              <Link to={`${path}/ownerEnroll`}>
+                <EnrollButton>회원가입</EnrollButton>
+              </Link>
+            </EnrollContainer>
+          </LoginContainer>
+        </ContainerWrapper>
+      </Container>
     </>
   );
 };
